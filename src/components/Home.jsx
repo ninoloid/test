422ae15f1b857f96236848686bc9c02464319208
ninoloid <muhammadsatriaadiputra@gmail.com>
2023-12-06 13:24:54 +0700

    fix scroll

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-scroll";
 
 const Home = () => {
   return (
-    <div name="home" className="h-screen w-full bg-[rgb(171,175,124)]">
+    <div name="Home" className="h-screen w-full bg-[rgb(171,175,124)]">
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-6xl font-bold text-yellow-400">
